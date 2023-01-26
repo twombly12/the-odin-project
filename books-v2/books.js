132e53169c201d3914f800bf8decf267cb996a4a
Ay Dee <amian.dimichele@gmail.com>
2023-01-25 22:46:19 -0500

    new book library - add submit book and delete book function

--- books-v2/books.js
+++ books-v2/books.js
@@ -15,37 +15,50 @@ function addBookToLibrary(title, author, pages, status) {
     bookLibrary.push(book)
 }
 
-
+// Test Values
+// Test Values
+// Test Values
+// Test Values
 
 addBookToLibrary("The Hobbit", "Tolkien", 452, "No")
 addBookToLibrary("The Hobbit", "Tolkien", 456, "No")
 addBookToLibrary("The Hobbit", "Tolkien", 654, "No")
 addBookToLibrary("The Hobbit", "Tolkien", 7852, "No")
 
+// Test Values
+// Test Values
+// Test Values
+// Test Values
 
-bookLibrary.forEach(element => {
+
+// Populate Book Cards
+function addBookCards(cardNumber) {
+
     // Create Book Card
     const card = document.createElement('div');
     card.className = 'bookCard'
+    card.setAttribute("data-value", cardNumber);
 
     // Create Book Title
     const cardTitle = document.createElement('p')
-    cardTitle.innerText = element.title;
+    cardTitle.innerText = bookLibrary[cardNumber].title;
 
     // Create Book Author
     const cardAuthor = document.createElement('p')
-    cardAuthor.innerText = element.author;
+    cardAuthor.innerText = bookLibrary[cardNumber].author;
 
     // Create Book PAges
     const cardPages = document.createElement('p')
-    cardPages.innerText = element.pages;
+    cardPages.innerText = bookLibrary[cardNumber].pages;
 
     // Create Book Status
     const cardStatus = document.createElement('p')
-    cardStatus.innerText = element.status;
+    cardStatus.innerText = bookLibrary[cardNumber].status;
 
     // Create Delete Button
     const cardDelete = document.createElement('button');
+    cardDelete.className = 'deleteBtn';
+    cardDelete.innerHTML = 'Delete';
 
     // Attach info to Card
     card.appendChild(cardTitle)
@@ -55,5 +68,71 @@ bookLibrary.forEach(element => {
     card.appendChild(cardDelete)
 
     // Attach Card to DOM
-    document.querySelector('#books').appendChild(card)
-})
+    document.querySelector('#bookCards').appendChild(card)
+}
+
+// Add All Books to Page
+function populateAllBooks() {
+    bookLibrary.forEach((element, index) => {
+        addBookCards(index)
+    })
+    deleteBook()
+}
+
+// Clear Books Container
+function clearBooksContainer() {
+    const bookCardContainer = document.querySelector('#bookCards');
+    bookCardContainer.innerHTML = '';
+}
+
+// Clear Form Values
+function clearFormValues() {
+    document.querySelector('#title').value = '';
+    document.querySelector('#author').value = '';
+    document.querySelector('#pages').value = '';
+    document.querySelector('#status').value = '';
+}
+
+// Add Form Values to bookLibrary
+function addToLibrary() {
+    const submit = document.querySelector('#addBook')
+    submit.addEventListener('click', function(event) {
+        event.preventDefault();
+        const title = document.querySelector('#title').value;
+        const author = document.querySelector('#author').value;
+        const pages = document.querySelector('#pages').value;
+        const status = document.querySelector('#status').value;
+
+        if (title === '' || author === '' || pages === '' || status === '') {
+            alert('Please fill out all fields')
+            return
+        }
+        if (isNaN(pages) == true) {
+            alert('Please enter a Number for Pages')
+            return
+        }
+        addBookToLibrary(title, author, pages, status);
+        addBookCards(bookLibrary.length - 1)
+        clearFormValues()
+    })
+}
+
+// Delete Book from Library
+function deleteBook() {
+    const deleteBtn = document.querySelectorAll('.deleteBtn');
+    deleteBtn.forEach(element => {
+        element.addEventListener('click', function(event) {
+            const card = this.closest('.bookCard').getAttribute('data-value')
+            console.log(card)
+            bookLibrary.splice(card, 1)
+            clearBooksContainer()
+            populateAllBooks()
+        })
+    })
+}
+
+
+// Run on Load
+
+populateAllBooks()
+addToLibrary()
